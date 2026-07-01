58bf2016671d08e420421641bbe1f6ac4a986a60
Lekhraj Patil <patillekhraj93@gmail.com>
2026-07-01 19:39:11 +0530

    update requirements

--- app/config.py
+++ app/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 # Load .env from parent directory
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = int(os.getenv("DB_PORT", 5432))
-DB_NAME = os.getenv("DB_NAME", "bi_platform")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Password")
+DB_HOST = "ep-green-breeze-at9hxpbl-pooler.c-9.us-east-1.aws.neon.tech"
+DB_PORT = 5432
+DB_NAME = "neondb"
+DB_USER = "neondb_owner"
+DB_PASSWORD = "npg_TvdFLCn29DoZ"
